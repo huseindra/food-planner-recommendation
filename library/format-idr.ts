87056a43d7a09303009850aa96e468c8b967713f
Husein <huseindra@gmail.com>
2024-07-28 02:34:09 +0700

    update: response message

---
+++ library/format-idr.ts
@@ -0,0 +1,8 @@
+export const formatIDR = (amount: number | string) => {
+    const number = typeof amount === 'string' ? parseFloat(amount) : amount;
+    return new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+      minimumFractionDigits: 0
+    }).format(number);
+  };
